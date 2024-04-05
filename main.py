5864a78a98dd91382651ff7817c67284676beffa
JasonDeAzevedo <163577961+JasonDeAzevedo@users.noreply.github.com>
2024-04-05 13:14:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,23 @@ with open(file_path, 'r') as fc:
     # csv_obj is an object as a result of the csv.reader(fc) function
     print(csv_text)
 
+'''
+Nathan
+Allow user to input genre likes
+'''
+
+'''
+(Omar Humeida) match user genre tastes with existing songs and their genre
+output a list of songs that fit their genres 
+'''
+
+'''
+(Jason) user can then ask the program to list songs based on an inputted genre
+allow user to filter songs by inputted year, output will change accordingly 
+(Jason) user can also choose to change the genre/year again
+'''
+
+'''
+(judah) user can add song/genre
+'''
+
